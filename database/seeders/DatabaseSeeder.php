cb6cd22f1bc32ee4f94754f99b45bbfaa1ace45b
Linchaker <linchaker@gmail.com>
2020-11-07 16:11:08 +0200

    config seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Products\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        Product::factory(50)->create();
+
     }
 }
