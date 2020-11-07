db676d1a1ac89e15f5603478c3560e70b0845fa8
Linchaker <linchaker@gmail.com>
2020-11-07 21:46:35 +0200

    add Warehouse factory

--- app/Models/Products/Warehouse.php
+++ app/Models/Products/Warehouse.php
@@ -4,13 +4,14 @@ namespace App\Models\Products;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Warehouse extends Model
 {
     use HasFactory;
 
-    public function products()
+    public function products(): BelongsToMany
     {
-        return $this->hasMany('App\Models\Products\Product');
+        return $this->belongsToMany(Product::class);
     }
 }
