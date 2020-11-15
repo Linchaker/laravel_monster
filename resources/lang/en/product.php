b4ec9535d5d0fdda92b34a40c1c296b4406ee709
Linchaker <linchaker@gmail.com>
2020-11-15 20:08:28 +0200

    add product en lang

---
+++ resources/lang/en/product.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'product' => 'Product',
+    'products' => 'Products',
+    'id' => 'ID',
+    'code' => 'Code',
+    'name' => 'Name',
+    'price' => 'Price',
+    'image_url' => 'Image',
+    'warehouses' => 'Warehouses',
+];
